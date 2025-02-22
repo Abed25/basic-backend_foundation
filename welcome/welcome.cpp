94a1ecf76419460d0e298ce7f690f557bd48bff4
Abed25 <officialwekesa254@gmail.com>
2025-02-22 10:26:59 +0300

    Welcome message from CPP file

--- welcome/welcome.cpp
+++ welcome/welcome.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include<string>
 // using namespace std;
+
+void welcome(std::string name){
+    std::cout << "Welcome to DSA crush with C++ "<< name;
+}
 int main (){
-    std::cout<<"hello world";
+    std::string myName;
+    std::cout<<"Who should i call you? :";
+    std::cin>>myName;
+    welcome(myName);
     return (0);
 }
